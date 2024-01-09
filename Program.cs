991dd6751a966d85cee029ec5eed61437b3fd09d
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-09 12:40:42 -0700

    demonstrates the use of reflection to get a method and attempt to invoke it, as well as the use of th

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace DYNAMIC 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           object obj = "Bravo";
+            //obj.GetHashCode()
+
+          var methodInfo = obj.GetType().GetMethod("get has code");
+            methodInfo.Invoke(null, null);
+
+            dynamic excelObject = "Bravo";
+            excelObject.Optimize();
+        }
+    }
+}
